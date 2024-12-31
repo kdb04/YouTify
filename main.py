82841ec4ea859c1710c5b793787cce83e502e09e
kdb04 <nikhil.srivatsa1@gmail.com>
2024-12-31 12:53:35 +0530

    search within playlist done

--- main.py
+++ main.py
@@ -162,6 +162,72 @@ def find_duplicates(driver, playlist_url):
     except Exception as e:
         print(f"Error finding duplicates: {str(e)}")
 
+def search_song(driver, playlist_url):
+    try:
+        query = input("Enter song name to search:").lower()
+
+        print("Opening playlist")
+        driver.get(playlist_url)
+        print("Waiting for page to load")
+        time.sleep(5)
+
+        scroll_playlist(driver)
+
+        songs = driver.find_elements(By.CSS_SELECTOR, "ytmusic-responsive-list-item-renderer")
+        print("\nProcessing playlist...")
+
+        songs_pos = defaultdict(list) #Storing song positions
+
+        for i, song in enumerate(songs, 1):
+            try:
+                title = None
+                artist = None
+
+                try:
+                    title = song.find_element(By.CSS_SELECTOR, "yt-formatted-string.title-column").text
+                except:
+                    try:
+                        title = song.find_element(By.CSS_SELECTOR, "[title]").get_attribute("title")
+                    except:
+                        pass
+
+                try:
+                    artist = song.find_element(By.CSS_SELECTOR, "yt-formatted-string.flex-column").text
+                except:
+                    try:
+                        res = song.find_elements(By.CSS_SELECTOR, "yt-formatted-string")
+                        if len(res)>1:
+                            artist = res[1].text
+                    except:
+                        pass
+
+                if title and artist:
+                    k = (title.lower(), artist.lower())
+                    songs_pos[k].append((i, f"{title}-{artist}"))
+
+            except Exception as e:
+                print(f"Error processing {i}: {str(e)}")
+
+            if i%50 == 0:
+                time.sleep(2)
+
+        found = False #flag to go through processed songs
+
+        for (title, artist), pos in songs_pos.items():
+            if query in title:
+                found = True
+                song_info = pos[0][1]
+
+                pos_list = [str(p[0]) for p in pos] #all positions where song present
+                print(f"\n{song_info}")
+                print(f"Found at positions: {','.join(pos_list)}")
+
+        if not found:
+            print("\nNot present in playlist")
+
+    except Exception as e:
+        print(f"Error searching for song: {str(e)}")
+
 def get_songs(driver, playlist_url):
     driver.get(playlist_url)
     time.sleep(5)
@@ -253,11 +319,12 @@ def display(driver, playlist_url):
         print("==YTM Manager==")
         print("1.List all songs")
         print("2.List duplicates in the playlist")
-        print("3.Start monitoring playlist")
-        print("4.Stop monitoring playlist")
-        print("5.Exit")
+        print("3.Search within playlist")
+        print("4.Start monitoring playlist")
+        print("5.Stop monitoring playlist")
+        print("6.Exit")
 
-        choice = int(input("Enter your choice(1-5):"))
+        choice = int(input("Enter your choice(1-6):"))
 
         if choice == 1:
             list_songs(driver, playlist_url)
@@ -268,20 +335,24 @@ def display(driver, playlist_url):
             input("Press enter to continue")
 
         elif choice == 3:
+            search_song(driver, playlist_url)
+            input("Press enter to continue")
+
+        elif choice == 4:
             if not notif_active.is_set():
                 notif_active.set()
                 print("Notif monitoring started")
             else:
                 print("Notif monitoring already active")
 
-        elif choice == 4:
+        elif choice == 5:
             if notif_active.is_set():
                 notif_active.clear()
                 print("Notif monitoring stopped")
             else:
                 print("Notif monitoring already stopped")
 
-        elif choice == 5:
+        elif choice == 6:
             notif_active.clear()
             print("Exiting program")
             driver.quit()
